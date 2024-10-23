3bb5540a6fd323e1affbf6e1d790bd0769b53910
RamdathDylan <126840011+RamdathDylan@users.noreply.github.com>
2024-10-23 19:15:23 -0400

    Create QuoteReader.java

---
+++ QuoteReader.java
@@ -0,0 +1,21 @@
+package com.example.javafxworkshop;
+
+public class QuoteReader extends Thread{
+    @Override
+    public void start() {
+
+
+
+
+        super.start();
+    }
+
+    @Override
+    public void run() {
+
+
+
+
+        super.run();
+    }
+}
